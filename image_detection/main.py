585da0c3adf89de5f8e348f91bcc92757aeac6e3
Mads Thede <madskt2002@gmail.com>
2024-05-23 09:16:13 +0200

    error logging implemented

--- image_detection/main.py
+++ image_detection/main.py
@@ -4,10 +4,12 @@ import numpy as np
 #import UARTfrom machine import UART
 import serial
 
+import csv
+
 #import math lib
 import math
 
-ser = serial.Serial("/dev/ttyACM0",9600)
+#ser = serial.Serial("/dev/ttyACM0",9600)
 
 # import kalman filter
 #from pykalman import KalmanFilter
@@ -23,6 +25,10 @@ ser = serial.Serial("/dev/ttyACM0",9600)
 
 #Encoder giver os de værdier vi ville forvente af systemet, ikke kun vinkler mindre en kameraets FOV
 #Det gør at hvis vores system får en actuator/saturator rent faktisk kan "cappe"
+
+
+
+fileName = str(input("Enter name of file: "))
 
 cap = cv2.VideoCapture(4)
 
@@ -44,6 +50,11 @@ test = True
 testxy = None
 colorRef = None
 
+xErrorFromBox = 0
+yErrorFromBox = 0
+frameCounter = 0
+
+
 #degree to radians
 
 bHorizontal = int(320/math.tan(math.radians(33)))
@@ -64,6 +75,8 @@ def getPixel(event, x, y, flags, param):
 
 while (test):
     testFrame = cap.read()[1]
+    #ksize = (10,10)
+    #testFrame = cv2.blur(testFrame,ksize)
     cv2.imshow('testFrame', testFrame)
 
     # call getPixel when clicking
@@ -95,8 +108,8 @@ print(colourSamples)
 #max hue
 #print(colourSamples[1][range(0, 2)])
 
-maxHue = np.amax(colourSamples[:, 0])
-minHue = np.amin(colourSamples[:, 0])
+maxHue = np.amax(colourSamples[:, 0])+0
+minHue = np.amin(colourSamples[:, 0])+0
 
 maxSat = np.amax(colourSamples[:, 1])
 minSat = np.amin(colourSamples[:, 1])
@@ -104,6 +117,7 @@ minSat = np.amin(colourSamples[:, 1])
 maxVal = np.amax(colourSamples[:, 2])
 minVal = np.amin(colourSamples[:, 2])
 
+maxHue += 10
 
 #print tolerances
 print(f"Hue tolerance: {maxHue-minHue}")
@@ -126,9 +140,11 @@ box_x_max, box_y_max = 320+boxx, 240+boxy
 
 
 while (True):
+    frameCounter += 1
     # Make live video feed
     frame = cap.read()[1]
-
+    #ksize = (10,10)
+    #frame = cv2.blur(frame,ksize)
     # Adjust color balance - new camera colour temperatures are too cold
     # b, g, r = cv2.split(frame)
     # r = r - 20
@@ -172,7 +188,7 @@ while (True):
                 maxCenter = center
 
     if maxRadius > 5:
-        print(f"max radius: {maxRadius}")   
+        #print(f"max radius: {maxRadius}")   
         cv2.circle(frame, maxCenter, maxRadius, (0, 255, 0), 2)
         measurementsx.append(maxCenter[0])
         measurementsy.append(maxCenter[1])
@@ -180,15 +196,26 @@ while (True):
         # Check if the measurements are within the box
         if maxCenter[0] < box_x_min or maxCenter[0] > box_x_max or maxCenter[1] < box_y_min or maxCenter[1] > box_y_max:
             # print x and y
-            print("X: ", maxCenter[0], "Y: ", maxCenter[1])
+            #print("X: ", maxCenter[0], "Y: ", maxCenter[1])
 
             # Check if we are too much to the right
             if maxCenter[0] > box_x_max:
                 turnAngleHorizontal = int(math.degrees(math.atan2((maxCenter[0] - frameCenter[0]), bHorizontal)))
                 print(f"Go right by {turnAngleHorizontal}")
-                #Send angle over UART
+                #Send angle over UART      
+                #ser.write((int(turnAngleHorizontal)).to_bytes(1, byteorder='big'))
                 
-                ser.write((int(turnAngleHorizontal)).to_bytes(1, byteorder='big'))
+                xErrorFromBox = maxCenter[0] - box_x_max
+                
+                
+                if(maxCenter[1]>box_y_max):
+                    yErrorFromBox = maxCenter[1]-box_y_max
+                elif(maxCenter[1]<box_y_min):
+                    yErrorFromBox = box_y_min - maxCenter[1]
+                else :
+                    yErrorFromBox = 0
+                
+                
                 
             # Check if we are too much to the left
             elif maxCenter[0] < box_x_min:
@@ -196,8 +223,25 @@ while (True):
                 print(f"Go left by {turnAngleHorizontal}")
                 #Send angle over UART
                 turnAngleHorizontal |= (1<<6)
-                ser.write((int(turnAngleHorizontal)).to_bytes(1, byteorder='big'))
+                #ser.write((int(turnAngleHorizontal)).to_bytes(1, byteorder='big'))
 
+                # Calculate error from box
+                
+                
+                xErrorFromBox = box_x_min - maxCenter[0]
+                
+                if(maxCenter[1]>box_y_max):
+                    yErrorFromBox = maxCenter[1]-box_y_max
+                elif(maxCenter[1]<box_y_min):
+                    yErrorFromBox = box_y_min - maxCenter[1]
+                else :
+                    yErrorFromBox = 0
+                    
+                    
+                
+            else:
+                xErrorFromBox = 0
+                yErrorFromBox = 0
                 
             # Check if we are too low to the bottom
             if maxCenter[1] > box_y_max:
@@ -205,14 +249,47 @@ while (True):
                 print(f"Go down by {turnAngleVertical}")
                 #Send angle over UART
                 turnAngleVertical |= (1<<7) | (1<<6)
-                ser.write((int(turnAngleVertical)).to_bytes(1, byteorder='big'))
+                #ser.write((int(turnAngleVertical)).to_bytes(1, byteorder='big'))
+                
+                # Calculate error from box
+                if(maxCenter[0]>box_x_max):
+                    xErrorFromBox = maxCenter[0] - box_x_max
+                elif(maxCenter[0]<box_x_min):
+                    xErrorFromBox = box_x_min - maxCenter[0]
+                else :
+                    xErrorFromBox = 0
+                    
+                yErrorFromBox = maxCenter[1] - box_y_max
                 
             elif maxCenter[1] < box_y_min:
                 turnAngleVertical = int(math.degrees(math.atan2((frameCenter[1] - maxCenter[1]), bVertical)))
                 print(f"Go up by {turnAngleVertical}")
                 #Send angle over UART
                 turnAngleVertical |= (1<<7)
-                ser.write((int(turnAngleVertical)).to_bytes(1, byteorder='big'))
+                #ser.write((int(turnAngleVertical)).to_bytes(1, byteorder='big'))
+                
+                #error from box
+                
+                if(maxCenter[0]>box_x_max):
+                    xErrorFromBox = maxCenter[0] - box_x_max
+                elif(maxCenter[0]<box_x_min):
+                    xErrorFromBox = box_x_min - maxCenter[0]
+                else :
+                    xErrorFromBox = 0
+                
+                yErrorFromBox = box_y_min - maxCenter[1]
+            
+            else :
+                xErrorFromBox = 0
+                yErrorFromBox = 0
+                
+            errorFromCenter = math.sqrt((maxCenter[0] - 320)**2 + (maxCenter[1] - 240)**2)
+    
+            with open(f'{fileName}.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                # Write errorFromCenter to the CSV file
+                writer.writerow([frameCounter, errorFromCenter, xErrorFromBox, yErrorFromBox])
+                
     else:
         measurementsx.clear()
         measurementsy.clear()
@@ -234,6 +311,8 @@ while (True):
     # draw line from center of image to center of object
     if maxCenter[0] != 0 and maxCenter[1] != 0:
         cv2.line(frame, (320, 240), (maxCenter[0], maxCenter[1]), (0, 255, 0), 2)
+        
+    
 
     cv2.imshow('frame', frame)
     if (cv2.waitKey(1) & 0xFF == ord('q')):
